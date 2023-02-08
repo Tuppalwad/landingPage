b1d8ba656285c961947d1c85fd66ff9723e4e195
tuppalwad <vtuppalwad@gmail.com>
2023-02-09 00:12:17 +0530

    test

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import IMGS from "../Assets/images/vision.png";
 import Imgss from "../Assets/images/smallleaf.webp";
 import "../style/home.css";
+import IMG2 from "../Assets/images/bg11.PNG";
+import IMG1 from "../Assets/images/bg111.png";
+
 function Home() {
   const commentsection = useRef(null);
   const gotocommentsection = () => {
@@ -34,6 +37,7 @@ function Home() {
       setNavbar(false);
     }
   };
+
   window.addEventListener("scroll", changeBackground);
 
   return (
@@ -42,13 +46,13 @@ function Home() {
         <nav
           className={
             navbar
-              ? "navbar navbar-expand-lg navbar-light bg-dark fixed-top"
-              : "navbar navbar-expand-lg navbar-light   fixed-top"
+              ? "navbar navbar-expand-lg  bg-light fixed-top "
+              : "navbar navbar-expand-lg    fixed-top margintop"
           }
         >
           <div className="container">
             <Link
-              className="navbar-brand"
+              className="navbar-brand logo"
               ref={homet}
               to="/"
               onClick={gotohome}
@@ -107,54 +111,93 @@ function Home() {
               <div className=" phone_mode_iconss d-flex">
                 <li className="nav-item ">
                   <a
-                    className="nav-link icon_navbar"
+                    className="nav-link icon_navbar topIcon"
                     href="https://www.facebook.com/people/UNICO-FOODS/100087934491728/?mibextid=LQQJ4d"
                     target="_blank"
                   >
-                    <i className="fa-brands fa-facebook  nav_itemss"> </i>
+                    <i className="fa-brands   fa-facebook  nav_itemss"> </i>
                   </a>
                 </li>
                 <li className="nav-item">
                   <a
-                    className="nav-link icon_navbar"
+                    className="nav-link icon_navbar topIcon"
                     href="https://www.instagram.com/invites/contact/?i=1wy4i12zjvrfa&utm_content=psi6h2r"
                     target="_blank"
                   >
-                    <i className="fa-brands fa-instagram   nav_itemss"></i>
+                    <i className="fa-brands  fa-instagram   nav_itemss"></i>
                   </a>
                 </li>
 
                 <li className="nav-item">
                   <a
-                    className="nav-link icon_navbar"
+                    className="nav-link icon_navbar topIcon"
                     href="https://www.linkedin.com/company/unico-foods/?viewAsMember=true"
                     target="_blank"
                   >
-                    <i className="fa-brands fa-linkedin nav_itemss"></i>
+                    <i className="fa-brands  fa-linkedin nav_itemss"></i>
                   </a>
                 </li>
                 <li className="nav-item">
                   <a
-                    className="nav-link icon_navbar"
+                    className="nav-link icon_navbar topIcon"
                     href="https://twitter.com/unico_foods"
                     target="_blank"
                   >
-                    <i className="fa-brands fa-twitter nav_itemss"></i>
+                    <i className="fa-brands  fa-twitter nav_itemss"></i>
                   </a>
                 </li>
+                <li className="nav-item">
+                  <a
+                    className="nav-link icon_navbar topIcon"
+                    href="https://api.whatsapp.com/send?phone=919900001845&text=Hey%20BlissClub"
+                    target="_blank"
+                  >
+                    <i
+                      class="fab  topIcons fa-whatsapp   "
+                      style={{ fontSize: "22px" }}
+                    ></i>
+                  </a>
+                </li>
+                {/* <li className="nav-item">
+                  <a
+                    className="nav-link icon_navbar"
+                    href="https://api.whatsapp.com/send?phone=919900001845&text=Hey%20BlissClub"
+                    class="btn-whatsapp-pulse"
+                    target="_blank"
+                  >
+                    <i
+                      class="fab fa-whatsapp "
+                      style={{ color: "white", fontSize: "20px" }}
+                    ></i>
+                  </a>
+                </li> */}
               </div>
             </div>
           </div>
         </nav>
         {/* section for vision hear start  */}
-        <section className="main">
-          <div className="container py-5 ">
-            <div className="row py-4">
-              <div className="col-md-7 pt-5 text-center"></div>
+
+        <div className="  ">
+          <div
+            id="carouselExampleSlidesOnly"
+            class="carousel slide"
+            data-bs-ride="carousel"
+          >
+            <div class="carousel-inner homedata">
+              <div class="carousel-item active ">
+                <img src={IMG1} class="d-block w-100 hompage" alt="..." />
+              </div>
+
+              <div class="carousel-item">
+                <img src={IMG2} class=" d-block w-100 hompage" alt="..." />
+              </div>
             </div>
           </div>
-        </section>
+        </div>
       </div>
+
+      {/* logo img  */}
+
       <section className="new">
         <div className="container mx-auto">
           <img
@@ -166,10 +209,6 @@ function Home() {
           />
         </div>
       </section>
-
-      {/* vision is ended  */}
-
-      {/* product is started  */}
 
       <section className="vision my-3">
         <div className="container">
@@ -202,7 +241,7 @@ function Home() {
       </section>
 
       <section className="about my-4">
-        <div className="container text-center">
+        <div className="container-fluid text-center">
           <h1>About Millet</h1>
           <img src={Imgss} className="mx-auto img_about" alt="" />
         </div>
@@ -211,7 +250,7 @@ function Home() {
             <div className="col-6">
               <img src="" alt="" />
             </div>
-            <div className="col-lg-6 my-4">
+            <div className="col-lg-6 my-2">
               Millet, a highly nutritious grain, is packed with antioxidants,
               protein, and fiber, making it the perfect base for a healthy
               breakfast. Whether you're looking for a hearty bowl of hot cereal,
@@ -240,8 +279,8 @@ function Home() {
           </h1>
         </div>
         <div className="container my-4 ">
-          <div className="row">
-            <div className="location my-3 col-lg-6">
+          <div className="row justify-content-between">
+            <div className="location my-3  col-lg-6">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7337.0514242449235!2d79.94418862345275!3d23.15100533955253!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3981add8e2210789%3A0x20145806faa4c93f!2sSadar%2C%20Jabalpur%2C%20Madhya%20Pradesh%20482001!5e0!3m2!1sen!2sin!4v1675790474049!5m2!1sen!2sin"
                 className="h-100 w-100"
@@ -250,8 +289,8 @@ function Home() {
                 loading="lazy"
               ></iframe>
             </div>
-            <div className="col-lg-6  ">
-              <form action="https://formspree.io/f/mbjeglej" method="post">
+            <div className="col-lg-5   ">
+              <form action="https://formspree.io/f/moqzejpl" method="post">
                 <div className="form-group my-2">
                   <div className="row">
                     <h5>Message Us</h5>
@@ -328,127 +367,178 @@ function Home() {
         </div>
       </section>
 
-      <section className="footer">
-        <div className="container-fluid my-5 footer_data">
+      <section className="container-fluid footer">
+        <div className=" my-5 footer_data">
           <footer
-            className="text-center myfooter text-lg-start text-white"
+            className=" text-center myfooter text-lg-start text-white"
             style={{
               backgroundColor: "#1c2331",
             }}
           >
-            <div className="container p-4 pb-0">
-              <section className="container">
-                <div className="row">
-                  <div className="col-md-3 col-lg-3 col-xl-3 mx-auto ">
-                    <h6 className="text-uppercase mb-4 my-5 font-weight-bold">
-                      <Link
-                        onClick={gotohome}
-                        to="/"
-                        style={{ color: "white", marginTop: "10px" }}
-                      >
-                        UNICO FOODS
-                      </Link>
-                    </h6>
-                    <p style={{ color: "#fff" }}>
-                      Welcome to the world of wholesome and nutritious
-                      breakfasts! At UNICO FOODS, we believe that the key to a
-                      great day starts with a nutritious and delicious
-                      breakfast. That's why we're proud to offer a wide variety
-                      of millet-based breakfast options that are sure to please
-                      even the most discerning palate.
-                    </p>
-                  </div>
+            <section className="container-fluid">
+              <div className="row">
+                <div className="col-md-3 col-lg-3 col-xl-3 mx-auto ">
+                  <h6 className="text-uppercase mb-4 my-5 font-weight-bold">
+                    <Link
+                      onClick={gotohome}
+                      to="/"
+                      style={{ color: "white", marginTop: "10px" }}
+                    >
+                      UNICO FOODS
+                    </Link>
+                  </h6>
+                  <p style={{ color: "#fff" }}>
+                    Welcome to the world of wholesome and nutritious breakfasts!
+                    At UNICO FOODS, we believe that the key to a great day
+                    starts with a nutritious and delicious breakfast. That's why
+                    we're proud to offer a wide variety of millet-based
+                    breakfast options that are sure to please even the most
+                    discerning palate.
+                  </p>
+                </div>
 
-                  <hr className="w-100 clearfix d-md-none" />
+                <hr className="w-100 clearfix d-md-none" />
 
-                  <div className="col-md-2 col-lg-2 col-xl-2 mx-auto  mt-2">
-                    <h6 className="text-uppercase mb-4 my-5 font-weight-bold">
-                      Link
-                    </h6>
+                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto  mt-2">
+                  <h6 className="text-uppercase mb-4 my-5 font-weight-bold">
+                    Link
+                  </h6>
 
-                    <p>
-                      <Link className="text-white" to="/" onClick={gotohome}>
-                        Home
-                      </Link>
-                    </p>
-                    <p>
-                      <Link
-                        className="text-white"
-                        to="/"
-                        onClick={gotocommentsection}
-                      >
-                        About
-                      </Link>
-                    </p>
-                    <p>
-                      <Link
-                        className="text-white"
-                        to="/"
-                        onClick={gotoscontactsection}
-                      >
-                        Contact
-                      </Link>
-                    </p>
-                  </div>
-
-                  <hr className="w-100 clearfix d-md-none" />
-
-                  <div className="col-md-3 col-lg-2 col-xl-2 mx-auto ">
-                    <h6 className="text-uppercase my-5  mb-4 font-weight-bold">
-                      Useful links
-                    </h6>
-                    <p>
-                      <a className="text-white">Your Account</a>
-                    </p>
-                    <p>
-                      <a className="text-white">Become an Affiliate</a>
-                    </p>
-                    <p>
-                      <a className="text-white">Shipping Rates</a>
-                    </p>
-                    <p>
-                      <a className="text-white">Help</a>
-                    </p>
-                    <p>
-                      <a className="text-white">Become our investres</a>
-                    </p>
-                  </div>
-
-                  <hr className="w-100 clearfix d-md-none" />
-
-                  <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-5">
-                    <h6 className="text-uppercase mb-4 font-weight-bold">
+                  <p>
+                    <Link className="text-white" to="/" onClick={gotohome}>
+                      Home
+                    </Link>
+                  </p>
+                  <p>
+                    <Link
+                      className="text-white"
+                      to="/"
+                      onClick={gotocommentsection}
+                    >
+                      About
+                    </Link>
+                  </p>
+                  <p>
+                    <Link
+                      className="text-white"
+                      to="/"
+                      onClick={gotoscontactsection}
+                    >
                       Contact
-                    </h6>
-
-                    <p>
-                      <i className="fas fa-home mr-3"></i> Unit no 224 AB lane 8
-                      Sadar cantt Jabalpur 482001
-                    </p>
-                    <p>
-                      <i className="fas fa-envelope mr-3"></i>{" "}
-                      Connect@unicofoods.in
-                    </p>
-                    <p>
-                      <i className="fas fa-phone mr-3"></i> +91 89896 35388
-                    </p>
-                    <p>
-                      <i className="fas fa-phone mr-3"></i> +91 96695 59988
-                    </p>
-                  </div>
+                    </Link>
+                  </p>
                 </div>
-              </section>
 
-              <hr className="my-3" />
+                <hr className="w-100 clearfix d-md-none" />
 
-              <section className="p-3 pt-0">
-                <div className="row d-flex align-items-center">
-                  <div className="col-md-7 col-lg-8 text-center text-md-start">
-                    <div className="">@ 2021 UNICO FOODS.</div>
-                  </div>
+                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto ">
+                  <h6 className="text-uppercase my-5  mb-4 font-weight-bold">
+                    Useful links
+                  </h6>
+                  <p>
+                    <a className="text-white">Your Account</a>
+                  </p>
+                  <p>
+                    <a className="text-white">Become an Affiliate</a>
+                  </p>
+                  <p>
+                    <a className="text-white">Shipping Rates</a>
+                  </p>
+                  <p>
+                    <a className="text-white">Help</a>
+                  </p>
+                  <p>
+                    <a className="text-white">Become our investres</a>
+                  </p>
                 </div>
-              </section>
-            </div>
+
+                <hr className="w-100 clearfix d-md-none" />
+
+                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-5">
+                  <h6 className="text-uppercase mb-4 font-weight-bold">
+                    Contact
+                  </h6>
+
+                  <p>
+                    <i className="fas fa-home mr-3"></i> Unit no 224 AB lane 8
+                    Sadar cantt Jabalpur 482001
+                  </p>
+                  <p>
+                    <i className="fas fa-envelope mr-3"></i>{" "}
+                    Connect@unicofoods.in
+                  </p>
+                  <p>
+                    <i className="fas fa-phone mr-3"></i> +91 89896 35388
+                  </p>
+                  <p>
+                    <i className="fas fa-phone mr-3"></i> +91 96695 59988
+                  </p>
+                </div>
+              </div>
+            </section>
+
+            <hr className="my-3" />
+
+            <section className="p-3 pt-0">
+              <div className="row ">
+                <div className=" col-lg-6  text-center text-md-start">
+                  <p>@ 2023 UNICO FOODS.</p>
+                </div>
+                <div className="col-lg-6 phonemodedata  d-flex">
+                  <li className="nav-item ">
+                    <a
+                      className="nav-link "
+                      href="https://www.facebook.com/people/UNICO-FOODS/100087934491728/?mibextid=LQQJ4d"
+                      target="_blank"
+                    >
+                      <i className="fa-brands fa-facebook  nav_itemss"> </i>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link "
+                      href="https://www.instagram.com/invites/contact/?i=1wy4i12zjvrfa&utm_content=psi6h2r"
+                      target="_blank"
+                    >
+                      <i className="fa-brands fa-instagram   nav_itemss"></i>
+                    </a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a
+                      className="nav-link "
+                      href="https://www.linkedin.com/company/unico-foods/?viewAsMember=true"
+                      target="_blank"
+                    >
+                      <i className="fa-brands fa-linkedin nav_itemss"></i>
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      className="nav-link "
+                      href="https://twitter.com/unico_foods"
+                      target="_blank"
+                    >
+                      <i className="fa-brands fa-twitter nav_itemss"></i>
+                    </a>
+                  </li>
+
+                  <li className="nav-item">
+                    <a
+                      className="nav-link icon_navbar"
+                      href="https://api.whatsapp.com/send?phone=919900001845&text=Hey%20BlissClub"
+                      class="btn-whatsapp-pulse"
+                      target="_blank"
+                    >
+                      <i
+                        class="fab fa-whatsapp "
+                        style={{ color: "white", fontSize: "20px" }}
+                      ></i>
+                    </a>
+                  </li>
+                </div>
+              </div>
+            </section>
           </footer>
         </div>
       </section>
